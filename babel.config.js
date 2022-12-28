59d96aaf435f720f5723fcf5ac65affa1a17ac5a
Liangyu <rilyu@sina.com>
2022-12-28 14:29:11 +0800

    fix bug

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-private-methods', {loose: true}],
     ["@babel/plugin-proposal-class-properties", {loose: true}],
+    ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
     "@babel/plugin-transform-runtime"
   ],
 };
